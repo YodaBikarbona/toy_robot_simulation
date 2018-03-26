d895849fc06d708c09b22aa246a81544628bc1c2
Mihael <mihael@Mihaels-MacBook-Air.local>
2018-03-26 19:28:34 +0200

    toy_robot_simulation

--- test_toy_robot_simulation.py
+++ test_toy_robot_simulation.py
@@ -11,8 +11,15 @@ class TestRobotSimulation(unittest.TestCase):
     	self.test_robot = RobotPosition(x_position=3, y_position=3, direction='NORTH')
     
     def test_robot_set_position(self):
-    	import pdb;pdb.set_trace()
     	self.assertEqual(self.test_robot.x_position, 3)
+    
+    def test_robot_move(self):
+    	self.test_robot.move_robot(Table=self.test_table)
+    	self.assertEqual(self.test_robot.y_position, 4)
+    
+    def test_robot_rotate(self):
+    	self.test_robot.rotate_robot(command="LEFT")
+    	self.assertEqual(self.test_robot.direction, "WEST")
 
 
 if __name__ == '__main__':
